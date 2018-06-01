1d8cc57e0cd4526087f67d1061dc0c9ac866e790
Sunil <sunil9766@gmail.com>
2018-06-01 12:29:30 +0530

    added form component

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { Form } from '../../components/Article';
+
 class Home extends React.Component {
   render() {
     return(
       <div className='container'>
-        <h1>React-Express</h1>
+        <div className="row pt-5">
+          <div className="col-12 col-lg-6 offset-lg-3">
+            <h1 className="text-center">React-Express</h1>
+          </div>
+          <Form />
+        </div>
       </div>
     );
   }
